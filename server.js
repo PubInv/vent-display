2aa68475a324c7de318764db79ee2974ebe9c96a
Ben Coombs <bencoombs@protonmail.com>
2021-02-02 11:54:52 +1300

    Added support for PIRCS

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const cors = require('cors');
 var app = express();
+app.use(cors());
 const SerialPort = require('serialport'); //https://serialport.io/docs/guide-usage
 const Readline = require('@serialport/parser-readline');
 
@@ -42,6 +44,70 @@ app.get('/api/set', function(req, res) {
 		}
 	});
 });
+
+// /api/pircs?com=C&par=P&int=T&mod=0&val=400
+app.get('/api/pircs', function(req, res) {
+	var err = '';
+    var x = '{ ';
+	if (req.query.com){
+		x += '"com" : "' + req.query.com + '" , ';
+	} else {
+		err += "com not defined! ";
+	}
+	if (req.query.par){
+		x += '"par" : "' + req.query.par + '" , ';
+	} else {
+		err += "par not defined! ";
+	}
+	if (req.query.int){
+		x += '"int" : "' + req.query.int + '" , ';
+	} else {
+		err += "int not defined! ";
+	}
+	if (req.query.mod){
+		x += '"mod" : ' + req.query.mod + ' , ';
+	} else {
+		err += "mod not defined! ";
+	}
+	if (req.query.val){
+		x += '"val" : ' + req.query.val;
+	} else {
+		err += "val not defined! ";
+	}
+	x += ' }\n';
+
+	if (err.length > 0){
+		err += "\n"
+		res.setHeader("Content-Type", "text/plain");
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.status(400).send(err);
+	} else {
+	
+		res.setHeader("Content-Type", "application/json");
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.status(200).send(x);
+			
+		port.write(x, (err) => {
+			if (err) {
+			return console.log('Error on write: ', err.message);
+			}
+		});
+	}
+
+	// { "com" : "C" , "par" : "P" , "int" : "T" , "mod" : 0 , "val" : 400 }
+});
+
+// /api/pircs2/C/P/T/0/400
+app.get('/api/pircs2/:com/:par/:int/:mod/:val', function(req,res) {
+	res.send(req.params);
+	port.write(JSON.stringify(req.params)+'\n', (err) => {
+		if (err) {
+		  return console.log('Error on write: ', err.message);
+		}
+	});
+
+	// JSON.stringify returns: {"com":"C","par":"P","int":"T","mod":"0","val":"400"}
+})
 
 /*app.get('/rr/:rr', function(req,res) {
 	res.send(req.params);
