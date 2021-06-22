072ea87b7575e0b7a4354fad47eb50370bb8df55
rajaniraju <rajaniraju2003@gmail.com>
2021-06-22 17:39:13 -0500

    Made responsive vent-output

--- vent-output.js
+++ vent-output.js
@@ -21,27 +21,40 @@ class VentOutput extends HTMLElement {
 				
 				border: solid 1px #105955;
 				background-color: #90D9D5;
-				border-radius: 2%;
+				border-radius: 1%;
 				cursor: default;
-				width:75%;
+				width:100%;
 				height:25%;
 				padding:2%;
-				margin: 1%;
+				
 			}
 			.box {
+				display:flex;
 				text-align:center;
 				font-size: 2em;
-				margin-bottom: 1px;
+				margin-bottom: 1%;
 				background-color: #DCDCDC;
-				border-radius: 5px;
-				border-width:1px;
-				width: 100px;
-				height: 50px;
-				padding:5px;
-				box-sizing: border-box;
-				size:100;
+				border-radius:1%;
+				border-width:0.5%;
+				width: 75%;
+				height: 60%;
+				padding:1%;
+				
 				
 			  }
+			.container{
+				display:flex;
+				flex-direction:row;
+				justify-content: space-between;
+				
+				flex-wrap: wrap;
+				width: 40%;
+				height: 25%;
+				padding:2%;
+				margin:1%;
+				border :1px ;
+
+			}
 		
 			.v {
 				font-size: 4em;
@@ -58,45 +71,31 @@ class VentOutput extends HTMLElement {
 			.labels {
 				display:flex;
 			}
-			.limit_max {
-				display: flex;
-				flex-direction: row;
-			  }
-			.limit_min {
-				display: flex;
-				flex-direction: row;
-			}	
+			
 		</style>
 		
 		<div class="vent-output">
-		<div class="flex-container">
-			<div> <span class="v">00</span>
-			<vent-input l="Pinsp" v="45" u="cmH2O"></vent-input>
-			<br>
-			<span class="l">label</span>
-			<span class="u">cmH2O</span>
-			</div>
-
 			<div class="flex-container">
-				<span class="vertical_alarms">
-				<div class="limit_max">
-                	<label for="max_h">H:</label>
-                	<input class="box" id="max_h" type='text' value=""> </input>
-                </div>
-		
-				<div class="limit_min">
-					<label for="max_l">L:</label>
-                    <input class="box" id="max_l" type='text'value="43"> </input>
-                </div>
+			<div class="container">
+				<div>
+				 <span class="v">00</span>
+				<vent-input l="Pinsp" v="45" u="cmH2O"></vent-input>
+				<br>
+				<span class="l">label</span>
+				<span class="u">cmH2O</span>
+				</div>
+			</div>	
 
+			<div class ="container">
+				
+				
+                	<div><label for="max_h">H:</label><input class="box" id="max_h" type='text' value="48"> </input></div>
+                	
+					
+             		<div><label for="max_l">L:</label><input class="box" id="max_l" type='text'value="43"> </input></div>
 
 			</div>
-		</div>  
-		
-			
-			
-			
-			
+		</div>  	
 		</div>		
 		`;
   }
